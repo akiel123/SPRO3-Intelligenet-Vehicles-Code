fd047603b1e22f15b0f0900f253b670ccabedce7
akiel123 <andreas.kielsgaard.code@gmail.com>
2016-09-14 11:13:08 +0200

    Project Initialisation
    Created atmel project

---
+++ SPRO3 Intelligent Vehicles/main.c
@@ -0,0 +1,18 @@
+/*
+ * SPRO3 Intelligent Vehicles.c
+ *
+ * Created: 14/09/2016 11:12:31 AM
+ * Author : Me
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
